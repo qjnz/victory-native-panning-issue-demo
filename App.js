c7ea028bd13b698896c97b90ffb0dc25b36db66f
Qiujie Li <liqiujie2008@gmail.com>
2021-05-13 08:41:34 +1200

    chore: remove unneeded imports

--- App.js
+++ App.js
@@ -1,28 +1,21 @@
-import React from "react"
-import { StyleSheet, View } from "react-native"
-import {
-  VictoryBar,
-  VictoryChart,
-  VictoryTheme,
-  VictoryZoomContainer,
-  VictoryStack,
-} from "victory-native"
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
 import { StackedBarDemo, NormalBarDemo } from './src'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <NormalBarDemo />
-      <StackedBarDemo />
-    </View>
-  )
+	return (
+		<View style={styles.container}>
+			<NormalBarDemo />
+			<StackedBarDemo />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#f5fcff",
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#f5fcff',
+	},
 })
